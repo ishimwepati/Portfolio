5bb099fe8b2f4228df616383c1f7130e8657f9f4
ishimwepati <wazacode@gmail.com>
2023-06-30 22:42:17 +0300

    preserved.js created

---
+++ preserved.js
@@ -0,0 +1,29 @@
+
+contactForm.addEventListener('submit', () => {
+    const storeData = {
+      storeName: name.value,
+      storeEmail: email.value,
+      storeMessage: message.value,
+    };
+  localStorage.setItem('storeData', JSON.stringify(storeData)); 
+  });
+  
+  window.onload = function dataLoad() {
+    const data = JSON.parse(localStorage.getItem('storeData'));
+    if (data) {
+      name.value = data.storeName;
+      email.value = data.storeEmail;
+      message.value = data.storeMessage;
+    }
+  };
+  
+//Preserve data from the Form
+
+window.onload = function dataLoad() {
+    const data = JSON.parse(localStorage.getItem('storeData'));
+    if (data) {
+      name.value = data.storeName;
+      email.value = data.storeEmail;
+      message.value = data.storeMessage;
+    }
+  };
